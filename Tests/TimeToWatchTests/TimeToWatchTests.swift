75fecd26382c6375f96692dad7feb3d94f9a339e
James Mark <james.david.mark@gmail.com>
2023-11-28 07:29:19 -0600

    Putting together most basic testing framework

---
+++ Tests/TimeToWatchTests/TimeToWatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by James Mark on 11/27/23.
+//
+import XCTest
+import Foundation
+
+@testable import TimeToWatch
+
+final class TimeToWatchwatchTests: XCTestCase {
+    func testTest() {
+        XCTAssertTrue(true)
+    }
+}
--- Tests/time-to-watchTests/Time-To-WatchTests.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by James Mark on 11/27/23.
-//
-
-import Foundation

b1c1dcbe30662afb3ad157516d18d14a82dc3e87
James Mark <james.david.mark@gmail.com>
2023-11-28 07:30:03 -0600

    Some refactoring around the package name

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "time-to-watch",
+    name: "TimeToWatch",
     dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
     ],
@@ -12,8 +12,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "time-to-watch", dependencies: [
+            name: "TimeToWatch", dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]),
+        .testTarget(
+            name: "TimeToWatchTests",
+            dependencies: ["TimeToWatch"]),
     ]
 )

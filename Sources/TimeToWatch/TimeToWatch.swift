48b44359f03d30f08e9151c4c672522a18751234
James Mark <james.david.mark@gmail.com>
2023-12-13 17:41:45 -0600

    Creating the Create command

--- Sources/TimeToWatch/TimeToWatch.swift
+++ Sources/TimeToWatch/TimeToWatch.swift
@@ -9,7 +9,7 @@ struct TimeToWatch: ParsableCommand {
         
         version: "0.0.1",
         
-        subcommands: [Add.self, List.self, Watch.self, Remove.self],
+        subcommands: [Create.self, Add.self, List.self, Watch.self, Remove.self],
         
         defaultSubcommand: List.self
     )
---
+++ readme.md
@@ -0,0 +1,3 @@
+# TimeToWatch
+
+
